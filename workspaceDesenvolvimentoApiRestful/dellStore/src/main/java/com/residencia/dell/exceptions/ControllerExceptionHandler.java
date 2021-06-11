e70854064ce69f2877f1280aef5241153adfa7d0
Marcos Paulo Marques Corrêa <marcos_barker@hotmail.com>
2021-06-11 15:23:27 -0300

    refactor: exceptions

--- workspaceDesenvolvimentoApiRestful/dellStore/src/main/java/com/residencia/dell/exceptions/ControllerExceptionHandler.java
+++ workspaceDesenvolvimentoApiRestful/dellStore/src/main/java/com/residencia/dell/exceptions/ControllerExceptionHandler.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-
 @ControllerAdvice
 public class ControllerExceptionHandler extends ResponseEntityExceptionHandler{
 
